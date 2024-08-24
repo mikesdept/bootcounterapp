1e3ff027a5a82b78d529bae249a3f3133e85fae5
Sergei Mikevich <mikesdepartment@gmail.com>
2024-08-24 11:32:33 +0200

    convert timestamp

--- presentation/src/main/java/mikes/dept/presentation/ui/booteventlist/adapter/BootEventViewHolder.kt
+++ presentation/src/main/java/mikes/dept/presentation/ui/booteventlist/adapter/BootEventViewHolder.kt
@@ -3,13 +3,28 @@ package mikes.dept.presentation.ui.booteventlist.adapter
 import androidx.recyclerview.widget.RecyclerView
 import mikes.dept.domain.entities.BootEventEntity
 import mikes.dept.presentation.databinding.ViewItemBootEventBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class BootEventViewHolder(
     private val viewItemBootEventBinding: ViewItemBootEventBinding
 ) : RecyclerView.ViewHolder(viewItemBootEventBinding.root) {
 
+    private companion object {
+        private const val DATE_FORMAT_PATTERN = "dd/MM/yyyy"
+        private const val UNDEFINED_DATE = "Undefined"
+    }
+
     fun bind(bootEventEntity: BootEventEntity) {
-        viewItemBootEventBinding.tvItem.text = bootEventEntity.timestamp.toString()
+        viewItemBootEventBinding.tvItem.text = bootEventEntity.timestamp.convertTimestamp()
+    }
+
+    private fun Long.convertTimestamp(): String {
+        return runCatching {
+            val sdf = SimpleDateFormat(DATE_FORMAT_PATTERN, Locale.getDefault())
+            return sdf.format(Date(this))
+        }.getOrElse { UNDEFINED_DATE }
     }
 
 }
